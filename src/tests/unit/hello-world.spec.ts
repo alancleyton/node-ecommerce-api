a296d4e0371fe68e17010352f5f182ac1fb88669
Alan Cleyton <alancleyton67@gmail.com>
2022-04-11 21:12:29 -0300

    fix husky pre-commit hook

--- src/tests/unit/hello-world.spec.ts
+++ src/tests/unit/hello-world.spec.ts
@@ -1,6 +1,6 @@
 describe('Hello World testing', () => {
   it('msg should contain Hello world', () => {
     const msg = 'Hello world';
-    expect(msg).toBe('Hello word');
+    expect(msg).toBe('Hello world');
   });
 });
