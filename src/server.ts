3933d9bf021b96d155959415ddfed324f1ec2abd
Alan Cleyton <alancleyton67@gmail.com>
2022-04-18 10:38:15 -0300

    refactor: create app module

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import app from './app';
 
-dotenv.config();
 const port = process.env.PORT || 3000;
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send({ msg: 'Hello World' });
-});
 
 app.listen(port, () => {
   console.log(`[\x1b[32mserver\x1b[0m] - server is running at: \x1b[36mhttp://localhost:${port}\x1b[0m`);
