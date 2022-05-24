fb9e04dfe03edd7e5664c31a3cabb2a879525caf
Alan Cleyton <60006501+alancleyton@users.noreply.github.com>
2022-05-24 16:08:45 -0300

    Feature/product entity (#2)
    * feat: create product entity
    
    * feat: add create product migration
    
    * chore: add gitattributes

---
+++ src/domain/product/product-entity.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  title: string;
+
+  @Column('varchar')
+  slug: string;
+
+  @Column('varchar')
+  description: string;
+
+  @Column({ nullable: false, type: 'float', default: 0.0 })
+  original_price: number;
+
+  @Column({ nullable: true, type: 'int', default: null })
+  discount: number;
+
+  @Column({ nullable: false, type: 'float', default: 0.0 })
+  discounted_price: number;
+
+  @Column({ nullable: false, type: 'int', default: 0 })
+  stock_amount: number;
+
+  @Column({ nullable: false, type: 'int', default: 0 })
+  sold: number;
+
+  @Column('timestamp')
+  created_at: Date;
+
+  @Column('timestamp')
+  updated_at: Date;
+}
